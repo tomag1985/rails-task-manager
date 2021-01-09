c0cb224cd0c42483bac003376baf08672a403402
tomas agnese <tagnese1985@gmail.com>
2021-01-09 15:54:38 -0300

    task manager with no style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #resources :tasks
+
+  # read
+  get 'tasks', to: 'tasks#index', as: :tasks # read
+  post 'tasks', to: 'tasks#create' # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task # create
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # update
+  get 'tasks/:id', to: 'tasks#show', as: :task # read
+  patch 'tasks/:id', to: 'tasks#update' # update
+  delete 'tasks/:id', to: 'tasks#destroy' # delete
 end
