2e853f92eead5dd1a5bd4fc3c9a7a3f2fc9c3e9f
tomas agnese <tagnese1985@gmail.com>
2021-01-11 23:10:44 -0300

    task manager with style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
 
+	before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@tasks = Task.all
 	end
 
-	def show
-		@task = Task.find(params[:id])
-	end
+	def show; end
 
 	def new
 		@task = Task.new
@@ -17,23 +17,23 @@ class TasksController < ApplicationController
 		redirect_to tasks_path
 	end
 
-	def edit
-		@task = Task.find(params[:id])
-	end
+	def edit; end
 
 	def update
-		@task = Task.find(params[:id])
-		@task.update(task_params)
+				@task.update(task_params)
 		redirect_to task_path(@task)
 	end
 
 	def destroy
-		@task = Task.find(params[:id])
 		@task.destroy
 		redirect_to tasks_path
 	end
 
 	private
+
+	def set_task
+		@task = Task.find(params[:id])
+	end
 
 	def task_params
 		params.require(:task).permit(:title, :details, :completed)
